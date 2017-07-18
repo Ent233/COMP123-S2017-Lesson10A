92ecc7c6e4e54e2e247cc23dc460e1b96639cdea
Ent233 <t6478990390@gmail.com>
2017-07-18 14:50:29 -0400

    Add project files.

---
+++ COMP123-S2017-Lesson10A/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+/*
+ * Name: Linlin Fan
+ * Date: July 13, 2017
+ * Description: Lab24 for Lesson 9
+ * Version: 0.8 - Modified Driver class to implement SuperHero
+ */
+
+namespace COMP123_S2017_Lesson10A
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            SuperHero superHero = new SuperHero("Super Dude", 20);
+            superHero.AddPower("Spider Climbing", 50);
+            superHero.AddPower("Webcasting", 40);
+            Console.WriteLine(superHero.ToString());
+
+            SuperVillain superVillain = new SuperVillain("Super Dude LV2", 20);
+            superVillain.AddPower("Spider Climbing LV2", 50);
+            superVillain.AddPower("Webcasting LV2", 40);
+            Console.WriteLine(superVillain.ToString());
+
+
+        }
+    }
+}
